e7554ba3d91396fd5072670f260a9ac1f1c36b04
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-03-31 19:29:24 +0200

    pep-8 fixes

--- pyxos/main.py
+++ pyxos/main.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
 import pykka
+import six
 
 from roles import replica
 
@@ -18,10 +31,10 @@ if __name__ == '__main__':
     start_replicas()
 
     ret = REPLICAS[0].ask({'command': 'get', 'key': 'x'}, timeout=3)
-    print 'Replica returned', ret
+    six.print_('Replica returned', ret)
 
     REPLICAS[0].tell({'command': 'set', 'key': 'x', 'value': '1'})
     ret = REPLICAS[0].ask({'command': 'get', 'key': 'x'}, timeout=3)
-    print 'Replica returned', ret
+    six.print_('Replica returned', ret)
 
     pykka.ActorRegistry.stop_all()
